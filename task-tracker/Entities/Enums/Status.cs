7058a127c0295a6f30368c8ee7e63b81c69f0661
Ronald <134448744+AndRonald@users.noreply.github.com>
2026-09-06 14:02:19 -0300

    add task entity and generic enum/enumextensions

---
+++ task-tracker/Entities/Enums/Status.cs
@@ -0,0 +1,48 @@
+﻿
+using System.ComponentModel;
+using System.Reflection;
+
+namespace task_tracker.Entities.Enums
+{
+    public enum Status
+    {
+        [Description("To Do")]
+        todo = 0,
+        [Description("In Progress")]
+        progress = 1,
+        [Description("Done")]
+        done = 2
+    }
+
+    public static class EnumExtensions
+    {
+        public static IList<T> EnumToList<T>()
+        {
+            if (!typeof(T).IsEnum)
+                throw new Exception("T isn't an enum type");
+
+            IList<T> list = new List<T>();
+            Type type = typeof(T);
+            if (type != null)
+            {
+                Array enumValues = Enum.GetValues(type);
+                foreach (T value in enumValues)
+                {
+                    list.Add(value);
+                }
+            }
+            return list;
+        }
+        public static string GetDescription(Enum item)
+        {
+            Type type = item.GetType();
+            FieldInfo fi = type.GetField(item.ToString());
+            DescriptionAttribute[] attributes = fi.GetCustomAttributes(typeof(DescriptionAttribute), false) as DescriptionAttribute[];
+            if (attributes.Length > 0)
+                return attributes[0].Description;
+            else
+                return String.Empty;
+        }
+    }
+
+}
---
+++ task-tracker/Entities/Task.cs
@@ -0,0 +1,13 @@
+﻿using task_tracker.Entities.Enums;
+
+namespace task_tracker.Entities
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string? Description { get; set; }
+        public Status Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdateAt { get; set; }
+    }
+}
